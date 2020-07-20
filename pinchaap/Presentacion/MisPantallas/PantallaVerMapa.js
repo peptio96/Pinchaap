31560ae9ba2492428bbfa87597b6542498a0b953
Alejandro Ruiz-Olalla <alejandroruizolallainfrp12@gmail.com>
2020-07-20 19:31:05 +0200

    mapview

--- pinchaap/Presentacion/MisPantallas/PantallaVerMapa.js
+++ pinchaap/Presentacion/MisPantallas/PantallaVerMapa.js
@@ -1,15 +1,36 @@
 import React from 'react'
-import {View} from 'react-native'
+import {View, StyleSheet} from 'react-native'
 import BotonPersonal from '../AdministrarPantallas/BotonPersonal'
 import { NavigationContext } from '@react-navigation/native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+ });
 
 class PantallaVerMapa extends React.Component {
   static contextType = NavigationContext;
   render(){
     const navigation = this.context;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', borderWidth: 0, borderColor: 'teal', backgroundColor: '#fff'}}>
-        <BotonPersonal colorFondoBoton='#fff' colorTextoBoton='orange' nombre='Go back' accion={() => navigation.goBack()}/>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+     </MapView>
       </View>
     )
   }
